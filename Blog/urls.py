56500bd147957bf382a13caf5f82ea4da317640a
NasarTarique <nasartarique@gmail.com>
2020-06-22 16:38:52 +0530

    layout

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('categories', views.categories, name="categories"),
+    path('<int:num>', views.read, name="read"),
+
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "Blog/index.html")
+
+
+def categories(request):
+    return render(request, "Blog/categories.html")
+
+
+def read(request, num):
+    return render(request, "Blog/reader.html")
