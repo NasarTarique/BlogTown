e0bfb482c72543cecd489455d4bf74ef3f679986
shubham mittal <shubhammittal175@gmail.com>
2020-08-10 22:12:58 +0530

    aboutzz

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categories', views.categories, name="categories"),
     path('<int:num>', views.read, name="read"),
+    path('about', views.about, name="about"),
 
 ]
