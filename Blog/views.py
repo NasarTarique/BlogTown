d26b9b53cc5621594cb72a20d46d672174e76c1b
NasarTarique <nasartarique@gmail.com>
2020-08-21 12:21:59 +0530

    viewspy changes

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+import ssl
+from smtplib import SMTP
+from . import config 
+
 from .models import Blog
 # Create your views here.
 
@@ -10,6 +15,7 @@ def index(request):
 
 def categories(request):
     return render(request, "Blog/categories.html")
+
 
 def about(request):
     return render(request, "Blog/about.html")    
@@ -26,10 +32,25 @@ def read(request, num):
         "posts": posts
     })
 
+
 def contact(request):
+
+    def send_mail(name, email, content):
+        context = ssl.create_default_context()
+        with SMTP("smtp.gmail.com") as smtp:
+            smtp.ehlo()
+            smtp.starttls(context=context)
+            smtp.ehlo()
+            subject = 'BlogTown Contact'
+            body = f"{name}\n\n{email}\n\n{content}" 
+            msg = f"Subject:{subject}\n\n{body}"
+            smtp.login(config.EMAIL_ADDRESS, config.EMAIL_PWD)
+            smtp.sendmail(config.EMAIL_ADDRESS, config.TEST_MAIL, msg)
+    
     if request.method == 'POST':
+        send_mail(request.POST.get('name'), request.POST.get('mail'), request.POST.get('content'))
         return render(request, "Blog/contact.html",{
-            "method":'post'
+            "method": 'post'
         })
     else:
         return render(request, "Blog/contact.html", {
